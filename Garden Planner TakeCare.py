3bfb58a1821b64f62b59710836b70395e730eb50
Arunabha Mukhopadhyay <145916970+Arunabha-Mukhopadhyay@users.noreply.github.com>
2023-11-24 18:09:26 +0530

    Update Garden Planner TakeCare.py

--- Garden Planner TakeCare.py
+++ Garden Planner TakeCare.py
@@ -5,43 +5,31 @@ garden_width = 10
 garden_height = 10
 garden = [["Empty" for _ in range(garden_width)] for _ in range(garden_height)]
 
-# Plant database with emojis and care instructions for various plants
+# Updated plant database with emojis, care instructions, and suitability conditions
 plant_database = {
     "Tomato": {
         "emoji": "🍅",
-        "care": "Tomatoes require full sun and regular watering. Use stakes for support as they grow."
+        "care": "Tomatoes require full sun and regular watering. Use stakes for support as they grow.",
+        "suitable_weather": ['Sunny'],
+        "suitable_soil": ['Loamy', 'Sandy']
     },
     "Carrot": {
         "emoji": "🥕",
-        "care": "Carrots need well-drained soil and consistent watering. Thin seedlings to avoid crowding."
+        "care": "Carrots need well-drained soil and consistent watering. Thin seedlings to avoid crowding.",
+        "suitable_weather": ['Sunny', 'Cloudy'],
+        "suitable_soil": ['Loamy']
     },
-    "Potato": {
-        "emoji": "🥔",
-        "care": "Plant potatoes in loose, well-draining soil. Hill the soil around the plants as they grow."
-    },
-    "Lettuce": {
-        "emoji": "🥬",
-        "care": "Lettuce prefers partial shade and consistent moisture. Harvest outer leaves for continued growth."
-    },
-    "Cucumber": {
-        "emoji": "🥒",
-        "care": "Cucumbers need ample sunlight and regular watering. Provide support for climbing varieties."
-    },
-    "Bell Pepper": {
-        "emoji": "🫑",
-        "care": "Bell peppers require full sun and regular watering. Pick them when they reach desired size and color."
-    },
-    "Eggplant": {
-        "emoji": "🍆",
-        "care": "Eggplants need warm temperatures and well-drained soil. Harvest fruits before they become too mature."
-    },
-    "Zucchini": {
-        "emoji": "🥒",
-        "care": "Zucchinis require consistent watering and fertile soil. Harvest them when they're young for best flavor."
-    },
-    # Add more plants with emojis and care instructions as needed
+    # Add more plants with emojis, care instructions, and suitability conditions as needed
 }
 
+# Function to get suitable plants based on weather and soil conditions
+def get_suitable_plants(weather, soil):
+    suitable_plants = []
+    for plant, info in plant_database.items():
+        if 'suitable_weather' in info and 'suitable_soil' in info:
+            if weather in info['suitable_weather'] and soil in info['suitable_soil']:
+                suitable_plants.append(plant)
+    return suitable_plants
 
 # Function to display the care instructions for a selected plant
 def display_care_instructions(event=None):
@@ -65,23 +53,26 @@ def display_garden():
                 cell_label = tk.Label(main_frame, text="Empty", borderwidth=1, relief="solid", width=8, height=2)
                 cell_label.grid(row=y, column=x)
 
-# Function to plant a selected plant in an entire row or column
+# Updated function to plant suitable crops in an entire row or column based on conditions
 def plant_specific_plants():
-    selected_plant = plant_listbox.get(tk.ACTIVE)
+    selected_weather = weather_dropdown.get()
+    selected_soil = soil_dropdown.get()
+    selected_plants = get_suitable_plants(selected_weather, selected_soil)
+
     row = int(row_entry.get())
     column = int(column_entry.get())
 
     planting_choice = planting_choice_var.get()  # Get planting choice (row or column)
 
-    if selected_plant in plant_database:
+    for plant in selected_plants:
         if planting_choice == "Row":
             for x in range(garden_width):
                 if garden[row][x] == "Empty":
-                    garden[row][x] = selected_plant
+                    garden[row][x] = plant
         elif planting_choice == "Column":
             for y in range(garden_height):
                 if garden[y][column] == "Empty":
-                    garden[y][column] = selected_plant
+                    garden[y][column] = plant
 
     # Update the garden display
     display_garden()
@@ -129,6 +120,27 @@ plant_specific_button.grid(row=4, column=0, columnspan=2)
 
 care_instructions_label = tk.Label(main_frame, text="Care Instructions for selected plant will be displayed here.", bg="#DAE0E6", font=("Arial", 12), fg="black")
 care_instructions_label.grid(row=5, column=0, columnspan=2)
+
+# Create a frame for weather and soil conditions
+conditions_frame = tk.Frame(main_frame, pady=10)  # Adjust the padding as needed
+conditions_frame.grid(row=6, column=0, columnspan=2)
+
+weather_conditions = ['Sunny', 'Rainy', 'Cloudy', 'Windy']
+soil_conditions = ['Clay', 'Sandy', 'Loamy', 'Rocky']
+
+weather_label = tk.Label(conditions_frame, text="Select Weather Condition:", bg="#DAE0E6", font=("Arial", 14), fg="black")
+weather_label.grid(row=0, column=0)
+weather_dropdown = tk.StringVar(conditions_frame)
+weather_dropdown.set(weather_conditions[0])  # Set a default weather condition
+weather_menu = tk.OptionMenu(conditions_frame, weather_dropdown, *weather_conditions)
+weather_menu.grid(row=0, column=1)
+
+soil_label = tk.Label(conditions_frame, text="Select Soil Condition:", bg="#DAE0E6", font=("Arial", 14), fg="black")
+soil_label.grid(row=1, column=0)
+soil_dropdown = tk.StringVar(conditions_frame)
+soil_dropdown.set(soil_conditions[0])  # Set a default soil condition
+soil_menu = tk.OptionMenu(conditions_frame, soil_dropdown, *soil_conditions)
+soil_menu.grid(row=1, column=1)
 
 # Bind the function to the plant_listbox selection event
 plant_listbox.bind('<<ListboxSelect>>', display_care_instructions)
